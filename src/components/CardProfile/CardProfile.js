145991c01f7d52e882a0b7f6ae85cb7f7db98777
Alan G Sinimbú <ar.redael@gmail.com>
2019-09-20 16:53:59 -0300

    Erase comment

--- src/components/CardProfile/CardProfile.js
+++ src/components/CardProfile/CardProfile.js
@@ -70,14 +70,3 @@ export default class CardProfile extends React.Component {
     )
   }
 }
-
-/*
-<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-  #photography
-            </span>
-  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-    #travel
-            </span>
-  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-    #winter
-            </span>*/
